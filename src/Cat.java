0af317b21938fd3d10f5c2b5ca3c24084e87aba9
eebag <gabehohman@hotmail.com>
2021-02-22 19:50:38 -0700

    a masterpiece in the works

--- src/Cat.java
+++ src/Cat.java
@@ -7,9 +7,9 @@ public class Cat {
 	// pick a head dimension
 	private static final int HEAD_DIMENSION = 100;
 	// eyes will be about 1/4 from top of head and 1/4 from left
-	private static final int EYE_Y = HEAD_DIMENSION/4;
+	private static final int EYE_Y = HEAD_DIMENSION/16;
 	private static final int EYE_X = HEAD_DIMENSION/4;
-	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
+	private static final int EYE_SEPARATION = HEAD_DIMENSION/2;
 	// pick eye dimensions
 	private static final int EYE_HEIGHT = 200;
 	private static final int EYE_WIDTH = 10;
@@ -17,7 +17,7 @@ public class Cat {
 	private static final int MOUTH_HEIGHT = 10;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION*4;
 	// mouth starts about 40% from left edge of head
-	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
+	private static final int MOUTH_X = HEAD_DIMENSION/5 * -4;
 	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
 	
 	// draw will render the Cat on the Graphics object
@@ -44,6 +44,6 @@ public class Cat {
 		g2.setColor(Color.red);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Help Me", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("\' Abstract art \'", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
